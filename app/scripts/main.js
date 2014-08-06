2daa37a2a75c180ab2ec9029916c6ea849ba5a8c
stecov <stefan.cova@publicis-modem.fr>
2014-08-06 17:24:13 +0200

    dotsBackgroud onClick + H1/H2

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
 
     //var height = $(window).height();
     //var width  = $(window).width();
-    var $logo = $('#logo');
+    //var $logo = $('#logo');
     var $letters = $('.letter');
     var $B = $('#B');
     var $A = $('#A');
@@ -53,53 +53,26 @@ $(document).ready(function () {
     };
 
     var showContact = function () {
-        //$('.contact').velocity({ opacity: 1 }, { display: 'block' });
         $('.contact').velocity('slideDown', { delay: 0, duration: 500 }, { display: 'block' });
-        if(query === '?3'){
+        $('.csstransitions.no-touch').find('#logo').one('click', function () {
             dotsBackground();
-        }
+        });
     };
 
 
     var dotsBackground = function () {
 
-        /*****************
-         Summary
-         *****************/
-
         /* Watch the codecast to learn how this demo was made: https://www.youtube.com/watch?v=MDLiVB6g2NY&hd=1 */
-
-        /* This demo serves two purposes:
-         1) Act as Velocity's primary visual test (in addition to the unit and load tests).
-         2) Demonstrate all of Velocity's features.
-         3) Demonstrate the performance capabilties of the DOM; WebGL and Canvas are not used in this demo.
-         */
-
-        /* Intended demo behavior:
-         1) A message box fades out.
-         2) Dots are randomly assigned coordinates and opacities then translated and increased in opacity. This animation is then reversed.
-         3) Meanwhile, the dots' container has its perspective, rotateZ, and opacity animated in a loop with a delay.
-         4) Once the dot animation is complete, the message box fades back in.
-         */
 
         /*********************
          Device Detection
          *********************/
 
-        var isWebkit = /Webkit/i.test(navigator.userAgent),
-            isChrome = /Chrome/i.test(navigator.userAgent),
+        var isChrome = /Chrome/i.test(navigator.userAgent),
             isMobile = !!('ontouchstart' in window),
             isAndroid = /Android/i.test(navigator.userAgent),
             isIE = document.documentMode;
 
-        /******************
-         Redirection
-         ******************/
-
-        if (isMobile && isAndroid && !isChrome) {
-            alert('Although Velocity.js works on all mobile browsers, this 3D demo is for iOS devices or Android devices running Chrome only. Redirecting you to Velocity documentation.');
-            window.location = 'index.html';
-        }
 
         /***************
          Helpers
@@ -120,7 +93,6 @@ $(document).ready(function () {
         /* Differentiate dot counts based on roughly-guestimated device and browser capabilities. */
         var dotsCount = isMobile ? (isAndroid ? 40 : 60) : (isChrome ? 175 : 125),
             dotsHtml = '',
-            $count = $('#count'),
             $dots;
 
         for (var i = 0; i < dotsCount; i++) {
@@ -129,14 +101,12 @@ $(document).ready(function () {
 
         $dots = $(dotsHtml);
 
-        $count.html(dotsCount);
 
         /*************
          Setup
          *************/
 
-        var $container = $('#container'),
-            $browserWidthNotice = $('#browserWidthNotice');
+        var $container = $('#container');
 
         var screenWidth = window.screen.availWidth,
             screenHeight = window.screen.availHeight,
@@ -153,11 +123,6 @@ $(document).ready(function () {
         /* IE10+ produce odd glitching issues when you rotateZ on a parent element subjected to 3D transforms. */
         if (!isIE) {
             containerAnimationMap.rotateZ = [ 5, 0 ];
-        }
-
-        /* Ensure the user is full-screened; this demo's translations are relative to screen width, not window width. */
-        if ((document.documentElement.clientWidth / screenWidth) < 0.80) {
-            $browserWidthNotice.show();
         }
 
         /*****************
@@ -195,21 +160,25 @@ $(document).ready(function () {
                     function () {
                         return r(translateZMin, translateZMax);
                     }
-                ]/*,
+                ],
                  opacity: [
-                 function () {
-                 return Math.random()
-                 },
-                 function () {
-                 return Math.random() + 0.1
-                 }
-                 ]*/
-            }, { duration: 6000 })
-            .velocity('reverse', { easing: 'easeOutQuad' })
-            .velocity({ opacity: 0 }, { duration: 2000, complete: function () {
-                console.log('anim FINISH');
-            }
+                     function () {
+                        return Math.random();
+                     },
+                     function () {
+                        return Math.random() + 0.1;
+                     }
+                 ]
+            }, {
+                duration: 10000,
+                complete: function () {
+                    $container.velocity({ opacity: 0 }, { duration: 5000, complete: function () {
+                            console.log('anim FINISH');
+                        }
+                    });
+                }
             })
+            .velocity('reverse', { easing: 'easeOutQuad' })
             .appendTo($container);
 
     };
